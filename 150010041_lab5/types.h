173cd2127162e189fe2289fae0980264428cc933
Anmol Mishra <anmolmishra1997@gmail.com>
2018-09-17 00:36:11 +0000

    Lab5 Complete

---
+++ 150010041_lab5/types.h
@@ -0,0 +1,9 @@
+// Signal numbers
+#define SIGNAL_PAUSE    	2
+#define SIGNAL_CONTINUE    	1
+#define SIGNAL_KILL    		3
+
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+typedef uint pde_t;
